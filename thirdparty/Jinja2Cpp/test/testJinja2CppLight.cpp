5a6f023d0dea4980e6b56b9035bf67e426d7b39c
Flex Ferrum <flexferrum@gmail.com>
2018-04-24 01:11:33 +0300

    Basic implementation of Jinja2 templates library

--- thirdparty/Jinja2Cpp/test/testJinja2CppLight.cpp
+++ thirdparty/Jinja2Cpp/test/testJinja2CppLight.cpp
@@ -8,150 +8,13 @@
 #include <string>
 
 #include "gtest/gtest.h"
-#include "test/gtest_supp.h"
 
-#include "Jinja2CppLight.h"
+#include "template.h"
 
 using namespace std;
-using namespace Jinja2CppLight;
+using namespace jinja2;
 
-TEST( testJinja2CppLight, basicsubstitution ) {
-    string source = R"DELIM(
-        This is my {{avalue}} template.  It's {{secondvalue}}...
-        Today's weather is {{weather}}.
-    )DELIM";
-
-    Template mytemplate( source );
-    mytemplate.setValue( "avalue", 3 );
-    mytemplate.setValue( "secondvalue", 12.123f );
-    mytemplate.setValue( "weather", "rain" );
-    string result = mytemplate.render();
-    cout << result << endl;
-    string expectedResult = R"DELIM(
-        This is my 3 template.  It's 12.123...
-        Today's weather is rain.
-    )DELIM";
-    EXPECT_EQ( expectedResult, result );
-}
-
-TEST( testJinja2CppLight, nestedsubstitution ) {
-    string source = R"DELIM(
-        This is my {{avalue}} template.  It's {{secondvalue}}...
-        Today's temperature is {{weather.degrees}}.
-    )DELIM";
-
-    Template mytemplate( source );
-    mytemplate.setValue( "avalue", 3 );
-    mytemplate.setValue( "secondvalue", 12.123f );
-    mytemplate.setValue( "weather", ValuesMap{{"kind", Value("rain")}, {"degrees", Value(21.5)}} );
-    string result = mytemplate.render();
-    cout << result << endl;
-    string expectedResult = R"DELIM(
-        This is my 3 template.  It's 12.123...
-        Today's temperature is 21.5.
-    )DELIM";
-    EXPECT_EQ( expectedResult, result );
-}
-TEST( testSpeedTemplates, loop ) {
-    string source = R"DELIM(
-{% for i in its %}
-    a[{{i}}] = image[{{i}}];
-{% endfor %}
-    )DELIM";
-
-    Template mytemplate( source );
-    mytemplate.setValue( "its", ValuesList{0, 1, 2} );
-    string result = mytemplate.render();
-    cout << result << endl;
-    string expectedResult = R"DELIM(
-
-    a[0] = image[0];
-
-    a[1] = image[1];
-
-    a[2] = image[2];
-
-    )DELIM";
-    EXPECT_EQ( expectedResult, result );
-}
-
-TEST( testSpeedTemplates, loopdata ) {
-    string source = R"DELIM(
-{% for i in its %}
-    {{loop}};
-{% endfor %}
-    )DELIM";
-
-    Template mytemplate( source );
-    mytemplate.setValue( "its", ValuesList{0, 1, 2} );
-    string result = mytemplate.render();
-    cout << result << endl;
-    string expectedResult = R"DELIM(
-
-    {{"length",3}, {"index",1}, {"last",False}, {"first",True}, {"index0",0}, {"nextitem",1}};
-
-    {{"length",3}, {"index",2}, {"last",False}, {"first",False}, {"index0",1}, {"previtem",0}, {"nextitem",2}};
-
-    {{"length",3}, {"index",3}, {"last",True}, {"first",False}, {"index0",2}, {"previtem",1}};
-
-    )DELIM";
-    EXPECT_EQ( expectedResult, result );
-}
-
-TEST( testSpeedTemplates, containerloop ) {
-    string source = R"DELIM(
-{% for i,name in images %}
-    a[{{i}}] = "{{name}}_{{loop.index0}}";
-{% endfor %}
-    )DELIM";
-
-    Template mytemplate( source );
-    mytemplate.setValue( "images", ValuesList{
-                             ValuesMap{{"i", Value(1)}, {"name", "image1"}},
-                             ValuesMap{{"i", Value(2)}, {"name", "image2"}},
-                             ValuesMap{{"i", Value(3)}, {"name", "image3"}},
-                         });
-    string result = mytemplate.render();
-    cout << result << endl;
-    string expectedResult = R"DELIM(
-
-    a[1] = "image1_0";
-
-    a[2] = "image2_1";
-
-    a[3] = "image3_2";
-
-    )DELIM";
-    EXPECT_EQ( expectedResult, result );
-}
-
-TEST( testSpeedTemplates, nestedloop ) {
-    string source = R"DELIM(
-{% for i in outers %}a[{{i}}] = image[{{i}}];
-{% for j in inners %}b[{{j}}] = image[{{j}}];
-{% endfor %}{% endfor %}
-)DELIM";
-
-    Template mytemplate( source );
-    mytemplate.setValue( "outers", ValuesList{0, 1, 2} );
-    mytemplate.setValue( "inners", ValuesList{0, 1} );
-    string result = mytemplate.render();
-    cout << "[" << result << "]" << endl;
-    string expectedResult = R"DELIM(
-a[0] = image[0];
-b[0] = image[0];
-b[1] = image[1];
-a[1] = image[1];
-b[0] = image[0];
-b[1] = image[1];
-a[2] = image[2];
-b[0] = image[0];
-b[1] = image[1];
-
-)DELIM";
-    EXPECT_EQ( expectedResult, result );
-}
-
+#if 0
 TEST(testSpeedTemplates, ifTrueTest) {
     const std::string source = "abc{% if True %}def{% endif %}ghi";
     Template mytemplate( source );
@@ -245,4 +108,5 @@ TEST(testSpeedTemplates, ifUnexpectedExpression) {
     }
     EXPECT_EQ(true, threw);
 }
+#endif
 
