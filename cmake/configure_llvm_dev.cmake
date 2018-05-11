1b4fb34f733851ba98425667bd5ac261cd012c8c
Flex Ferrum <flexferrum@gmail.com>
2018-05-12 01:20:18 +0300

    Refactor build scripts for windows platform

--- cmake/configure_llvm_dev.cmake
+++ cmake/configure_llvm_dev.cmake
@@ -18,6 +18,8 @@ message (STATUS "LLVM_ROOT calculated as '${LLVM_ROOT}'")
 set (LLVM_INSTALL_ROOT $ENV{LLVM_ROOT} CACHE PATH "Path to LLVM installation root directory")
 set (LLVM_SOURCES_ROOT $ENV{LLVM_ROOT} CACHE PATH "Path to LLVM sources root directory")
 
+message (STATUS "LLVM_INSTALL_ROOT calculated as '${LLVM_INSTALL_ROOT}'")
+
 set (CMAKE_CLANG_DIR ${CLANG_CMAKE_MODULES})
 
 find_path (
@@ -31,6 +33,8 @@ find_path (
         lib64/llvm-5.0/cmake
         lib/llvm/cmake/llvm
         lib/llvm/cmake
+        lib/cmake/llvm
+        lib/cmake
         lib64/llvm/cmake/llvm
         lib64/llvm/cmake
         share/llvm-5.0/cmake/llvm
@@ -39,28 +43,42 @@ find_path (
         share/llvm/cmake
 )
 
+set (CLANG_INCLUDE_DIRS)
+
 find_path (
     CLANG_INCLUDE_DIRS
-    AST.h
+    ASTConsumer.h
     PATHS ${LLVM_INSTALL_ROOT}
     PATH_SUFFIXES
         lib/llvm-5.0/include/clang/AST
         lib/llvm/include/clang/AST
+        lib/include/clang/AST
+        include/clang/AST
+    NO_DEFAULT_PATH
+    NO_CMAKE_ENVIRONMENT_PATH
+    NO_CMAKE_PATH
+    NO_SYSTEM_ENVIRONMENT_PATH
+    NO_CMAKE_SYSTEM_PATH
+    NO_CMAKE_FIND_ROOT_PATH
 )
 
 find_library (
     CLANG_CMAKE_LIBRARIES
-    clangBasic   
+    clangBasic
     PATHS ${LLVM_INSTALL_ROOT}
     PATH_SUFFIXES
         lib/llvm-5.0/lib
         lib64/llvm-5.0/lib
         lib/llvm/lib
         lib64/llvm/lib
+        lib
+        lib64
 )
 
 get_filename_component(CLANG_LIBRARY_DIRS "${CLANG_CMAKE_LIBRARIES}" PATH)
+message (STATUS "CLANG_INCLUDE_DIRS calculated as '${CLANG_INCLUDE_DIRS}'")
 get_filename_component(CLANG_INCLUDE_DIRS "${CLANG_INCLUDE_DIRS}" PATH)
+message (STATUS "CLANG_INCLUDE_DIRS calculated as '${CLANG_INCLUDE_DIRS}'")
 get_filename_component(CLANG_INCLUDE_DIRS "${CLANG_INCLUDE_DIRS}" PATH)
 
 message (STATUS "CLANG_CMAKE_LIBRARIES calculated as '${CLANG_CMAKE_LIBRARIES}'")
@@ -76,5 +94,11 @@ list (APPEND CMAKE_MODULE_PATH ${CLANG_CMAKE_MODULES})
 set (LLVM_CONFIG_HAS_RTTI YES)
 include(AddLLVM)
 set (LLVM_CONFIG_HAS_RTTI YES)
+
+if (WIN32 OR WIN64)
+    include (clang-windows/ClangConfig)
+else ()
+    include (clang-linux/ClangConfig)
+endif ()
 
 # message (FATAL_ERROR "Stop!")
