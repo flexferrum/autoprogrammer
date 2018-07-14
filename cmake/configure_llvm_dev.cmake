87a9dc8ff61c7bdd30fede249757b71984e4b954
Flex Ferrum <flexferrum@gmail.com>
2018-07-14 19:56:44 +0000

    Refactor build scripts

--- cmake/configure_llvm_dev.cmake
+++ cmake/configure_llvm_dev.cmake
@@ -43,6 +43,70 @@ find_path (
         share/llvm/cmake
 )
 
+#if (WINDOWS)
+#    set (LLVM_CONFIG_NAME "llvm-config.exe")
+#else ()
+
+set (LLVM_CONFIG_NAME "llvm-config" "llvm-config-5.0")
+
+# Taken from CppInsight project by Andreas Fertig
+# https://github.com/andreasfertig/cppinsights/blob/master/CMakeLists.txt
+
+find_program(
+    LLVM_CONFIG_PATH
+    NAMES ${LLVM_CONFIG_NAME}
+    PATHS ${LLVM_INSTALL_ROOT}
+    PATH_SUFFIXES
+        bin
+    )
+
+message (STATUS "LLVM_CONFIG_PATH calculated as '${LLVM_CONFIG_PATH}'")
+if(NOT LLVM_CONFIG_PATH)
+    message(FATAL_ERROR "llvm-config not found -- ${LLVM_CONFIG_PATH}: ${LLVM_CONFIG_NAME}")
+else()
+    message(STATUS "Found LLVM_CONFIG_PATH: ${LLVM_CONFIG_PATH}")
+endif()
+
+function(llvm_config VARNAME switch)
+    set(CONFIG_COMMAND "${LLVM_CONFIG_PATH}" "${switch}")
+
+    execute_process(
+        COMMAND ${CONFIG_COMMAND} ${LIB_TYPE}
+        RESULT_VARIABLE HAD_ERROR
+        OUTPUT_VARIABLE CONFIG_OUTPUT
+    )
+
+    if (HAD_ERROR)
+        string(REPLACE ";" " " CONFIG_COMMAND_STR "${CONFIG_COMMAND}")
+        message(STATUS "${CONFIG_COMMAND_STR}")
+        message(FATAL_ERROR "llvm-config failed with status ${HAD_ERROR}")
+    endif()
+
+    # replace linebreaks with semicolon
+    string(REGEX REPLACE
+        "[ \t]*[\r\n]+[ \t]*" ";"
+        CONFIG_OUTPUT ${CONFIG_OUTPUT})
+
+    # make all includes system include to prevent the compiler to warn about issues in LLVM/Clang
+    string(REGEX REPLACE "-I" "-isystem" CONFIG_OUTPUT "${CONFIG_OUTPUT}")
+
+    # remove certain options clang doesn't like
+    if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+        string(REGEX REPLACE "-Wl,--no-keep-files-mapped" "" CONFIG_OUTPUT "${CONFIG_OUTPUT}")
+        string(REGEX REPLACE "-Wl,--no-map-whole-files" "" CONFIG_OUTPUT "${CONFIG_OUTPUT}")
+        string(REGEX REPLACE "-fuse-ld=gold" "" CONFIG_OUTPUT "${CONFIG_OUTPUT}")
+    endif()
+
+    # make result available outside
+    set(${VARNAME} ${CONFIG_OUTPUT} PARENT_SCOPE)
+
+    # Optionally output the configured value
+    message(STATUS "llvm_config(${VARNAME})=>${CONFIG_OUTPUT}")
+
+    # cleanup
+    unset(CONFIG_COMMAND)
+endfunction(llvm_config)
+
 set (CLANG_INCLUDE_DIRS)
 
 find_path (
@@ -86,19 +150,37 @@ message (STATUS "CLANG_LIBRARY_DIRS calculated as '${CLANG_LIBRARY_DIRS}'")
 message (STATUS "CLANG_INCLUDE_DIRS calculated as '${CLANG_INCLUDE_DIRS}'")
 message (STATUS "LLVM_CMAKE_MODULES calculated as '${LLVM_CMAKE_MODULES}'")
 
-link_directories(${CLANG_LIBRARY_DIRS})
+link_directories(${CLANG_LIBRARY_DIRS} ${LLVM_LDFLAGS})
 
-list (APPEND CMAKE_MODULE_PATH ${LLVM_CMAKE_MODULES})
-list (APPEND CMAKE_MODULE_PATH ${CLANG_CMAKE_MODULES})
-
-set (LLVM_CONFIG_HAS_RTTI YES)
-include(AddLLVM)
 set (LLVM_CONFIG_HAS_RTTI YES)
 
 if (WIN32 OR WIN64)
+    message (STATUS " >>>>>>>> 1111111")
+    list (APPEND CMAKE_MODULE_PATH ${LLVM_CMAKE_MODULES})
+    include(AddLLVM)
     include (clang-windows/ClangConfig)
+#    set (EXTRA_LIBS LLVMCore LLVMMC LLVMSupport LLVMOption LLVMAnalysis LLVMBitReader LLVMBitWriter LLVMCore LLVMCoroutines LLVMCoverage LLVMipo LLVMIRReader LLVMInstCombine LLVMInstrumentation LLVMLTO LLVMLinker LLVMMC LLVMObjCARCOpts LLVMObject LLVMPasses LLVMProfileData LLVMScalarOpts LLVMSupport LLVMTarget LLVMTransformUtils)
 else ()
-    include (clang-linux/ClangConfig)
+    message (STATUS " >>>>>>>> 2222222")
+    llvm_config(LLVM_CXXFLAGS "--cxxflags")
+    llvm_config(LLVM_LDFLAGS "--ldflags")
+    llvm_config(LLVM_LIBS "--libs")
+    llvm_config(LLVM_LIBDIR "--libdir")
+    llvm_config(LLVM_INCLUDE_DIR "--includedir")
+    llvm_config(LLVM_SYSTEM_LIBS "--system-libs")
+    llvm_config(LLVM_PACKAGE_VERSION "--version")
+
+    string (REPLACE "-std=c++0x" "-std=c++14" LLVM_CXXFLAGS "${LLVM_CXXFLAGS}")
+    string (REPLACE "-std=c++11" "-std=c++14" LLVM_CXXFLAGS "${LLVM_CXXFLAGS}")
+    string (REPLACE "-fno-rtti" "" LLVM_CXXFLAGS "${LLVM_CXXFLAGS}")
+    string (REPLACE "-fno-exceptions" "" LLVM_CXXFLAGS "${LLVM_CXXFLAGS}")
+
+
+    macro(add_llvm_executable name)
+        add_executable( ${name} ${ARGN} )
+    endmacro(add_clang_executable)
+    set (EXTRA_LIBS ${LLVM_LDFLAGS} ${EXTRA_LIBS} ${LLVM_LIBS} ${LLVM_SYSTEM_LIBS})
 endif ()
+set (LLVM_CONFIG_HAS_RTTI YES)
 
 # message (FATAL_ERROR "Stop!")
